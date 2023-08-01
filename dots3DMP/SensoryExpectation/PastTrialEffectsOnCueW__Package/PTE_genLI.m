e774ebdb64fab77acd95438ac5b12bc8d22b2a86
Yared <yhaile2@jh.edu>
2023-08-01 18:01:53 -0400

    1 line function repair and comment cleanup!

--- dots3DMP/SensoryExpectation/PastTrialEffectsOnCueW__Package/PTE_genLI.m
+++ dots3DMP/SensoryExpectation/PastTrialEffectsOnCueW__Package/PTE_genLI.m
@@ -10,7 +10,7 @@ function LI = PTE_genLI(datastruct, fieldnames, nback, fieldValues)
 % Thus, you can see in the third cell of 'fieldValues' 2 values are given to associate with modality and PDW respectively.
 % 'nback' allows specification of whether you care about evaluating the data values on this trial (n=0) or past trials (n-1,2,3 etc.)
 
-numConditions = numel(fieldnames);
+numConditions = numel(fieldnames); % num columns in LI
 
     % Initialize output variable 'LI'
     if contains(fieldnames{1}, '&')
@@ -30,39 +30,25 @@ numConditions = numel(fieldnames);
             subfieldNames = strsplit(fieldName, '&');
             numSubfields = numel(subfieldNames);
             logicalVec = true(size(datastruct.(subfieldNames{1}))); % logicalVec = length(data) rows x 1 column
-            
-            % Calculate the maximum nback value among subfieldnames within the fieldname
-            % maxNback = max(nback{i});
 
-            % Evaluate each subfield against subfieldValue 
+            % Evaluate each subfield against subfieldValue to gen subfieldLI then compare via '&' each subfieldLI
             for j = 1:numSubfields
-                subfieldName = strtrim(subfieldNames{j});
+                subfieldName = strtrim(subfieldNames{j}); % remove any excess spaces around fieldName after split
                 subfieldValue = fieldValue(j);
                 Nback = n(j);
-                subfieldLogicalVec = datastruct.(subfieldName)(((Nback+1):length(datastruct.(subfieldName)))-Nback) == subfieldValue; % True when subfield-specific condition is met
-
+                subfieldLogicalVec = datastruct.(subfieldName)(((Nback+1):length(datastruct.(subfieldName)))-Nback) == subfieldValue; % True when subfield-specific condition is met, Nback shifts the whole vector back such that start postion is vector position 1
+                
                 % Pad the subfieldLogicalVec with false values for Nback rows
                 subfieldPadding = false(Nback, 1);
-                subfieldLogicalVec = [subfieldPadding; subfieldLogicalVec];
-                logicalVec = subfieldLogicalVec;
-%                 % Adjust the length of logicalVec if necessary
+                subfieldLogicalVec = [subfieldPadding; subfieldLogicalVec]; % re-aligned to full length of data vector
+                logicalVec = logicalVec & subfieldLogicalVec; % isolate instances where all subfield LI are true!
 % Assuming the data from which all subLogiVecs are formed is of equal
 % length all subLogiVecs should be same length. Subtract Nback values then
 % add back Nback 0s to the vector = take as much as given back = same as
 % original length.
-%                 subfieldLength = numel(subfieldLogicalVec);
-%                 logicalVec = logicalVec(1:subfieldLength) & subfieldLogicalVec;
             end
-
-%             % Pad the logicalVec with false values for maximum nback rows
-% This seems unnecessary since padding is occuring for each subfield
-% individually, according to it's specific nback value.
-%             padding = false(maxNback, 1);
-%             logicalVec = [padding; logicalVec];
-        else
-            % Field name does not contain '&', evaluate it directly
+        else % Field name does not contain '&', evaluate it directly
             logicalVec = datastruct.(fieldName)(((n+1):length(datastruct.(fieldName)))-n) == fieldValue; % True when field-specific condition is met
-
             % Pad the logicalVec with false values for nback rows
             padding = false(n, 1);
             logicalVec = [padding; logicalVec];
